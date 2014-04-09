461d8711d9cb0823f8bfe42db8188f2b4dac4b1c
Takayoshi Kimura <tkimura@redhat.com>
2014-04-09 10:20:50 +0900

    Add shell script to generate summary

---
+++ summary.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# GCVIEWER_JAR=/path/to/gcviewer.jar
+
+if [ -z $GCVIEWER_JAR ]; then
+    GCVIEWER_JAR=$1
+fi
+if [ -z $GCVIEWER_JAR ]; then
+    echo "Generate GC log summary using gcviewer. Please specify gcviewer.jar path in the first parameter."
+    echo "https://github.com/chewiebug/GCViewer"
+    echo ""
+    echo "Usage: $0 <gcviewer.jar>"
+    exit 1
+fi
+
+for f in `find . -name '*.log'`; do
+    java -jar $GCVIEWER_JAR $f $f-summary.txt
+done
+
